894c6efe1c953248257178f849c248d43d1a8c4a
Berkay2304 <berkaycansever23@gmail.com>
2025-07-01 19:39:59 +0300

    URL çağrıları render a göre ayarlandı

--- blogApp/src/api/axios.js
+++ blogApp/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://blog-app-q7fj.onrender.com/api',
 });
 
 // Her istekten önce token ekle
